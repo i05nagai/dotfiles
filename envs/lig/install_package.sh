86885cd65ab30bba7752212edd957fd50617fe1e
i05nagai <i05nagai@gmail.com>
2018-04-27 20:27:31 +0900

    Add lig/install_package.sh

---
+++ envs/lig/install_package.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+#
+# Install packages
+#
+
+# git
+sudo add-apt-repostory ppa:git-core/ppa
+sudo apt-get update
+sudo apt-get install -y git
+
+# chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get update
+sudo apt-get install -y google-chrome-stable
+
+
+# misc
+sudo apt-get install -y \
+  vim \
+  tmux \
+  zsh
+
+#
+# install
+#
+git submodule init
+git submodule update
+vim -N -u NONE -i NONE -V1 -e -s --cmd "source .vimrc" --cmd NeoBundleInstall! --cmd qall!
