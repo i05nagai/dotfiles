62f14ce4526d86b9a40981374651ff6cb8dc5ad0
i05nagai <i05nagai@gmail.com>
2018-08-30 11:50:20 +0900

    Update osx/brew.sh

--- envs/osx/brew.sh
+++ envs/osx/brew.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
 
-brew install tmux-mem-cpu-load
+# brew install tmux-mem-cpu-load
+brew install pass
+brew install vim --with-lua --with-gettext --with-override-system-vi
+brew install zplug
+brew install fzf
+brew install go
+brew install zsh
+brew install hub
+
+brew cask install docker
+brew cask install slack
+brew cask install dropbox
+brew cask install grammarly
+brew cask install google-chrome
+brew cask install mendeley
+brew cask install google-japanese-ime
+
+# terrafrom
+# java
+# hub
+# scala
+# gcloud
+
+#
+# Ricty
+#
+brew tap sanemat/font
+brew install ricty --with-powerline
+cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/
+fc-cache -vf
