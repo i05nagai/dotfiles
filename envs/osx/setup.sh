c38cb0af2ea98d3f686113db7206873dc879ed2c
i05nagai <i05nagai@gmail.com>
2018-08-30 11:49:58 +0900

    Add osx/setup.sh

---
+++ envs/osx/setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+
+#
+# conda
+#
+cd /tmp
+curl -L -O https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh
+bash Miniconda2-latest-MacOSX-x86_64.sh -b -p $HOME/miniconda2
+# conda create
+conda create -n default python=3.6
+# source activate default
+
+#
+# pip
+#
+pushd /tmp \
+  && curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py \
+  && python get-pip.py \
+  && popd
+
+#
+# powerline
+#
+pip install --user powerline-status
