33099c19594202aaa0dafb77a55a8969bb5d4b0f
i05nagai <i05nagai@gmail.com>
2019-04-28 22:23:23 +0200

    Update osx/brew.sh

--- envs/osx/brew.sh
+++ envs/osx/brew.sh
@@ -14,6 +14,14 @@ brew install pwgen
 brew install bazel
 brew install cmake
 
+# chunkwm
+brew tap koekeishiya/formulae
+brew install chunkwm
+brew services start chunkwm
+# skhd
+brew install skhd
+brew services start skhd
+
 brew cask install anaconda
 brew cask install iterm2
 brew cask install karabiner-elements
@@ -27,10 +35,16 @@ brew cask install google-chrome
 brew cask install mendeley
 brew cask install google-japanese-ime
 brew cask install skitch
+brew cask install intellij-idea-ce
 
 # terrafrom
 # scala
 # gcloud
+
+# miniconda2
+# curl -O -L https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
+curl -L -O https://repo.anaconda.com/miniconda/Miniconda2-latest-MacOSX-x86_64.sh
+bash Miniconda2-latest-MacOSX-x86_64.sh -b
 
 #
 # Ricty
@@ -39,3 +53,4 @@ brew tap sanemat/font
 brew install ricty --with-powerline
 cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/
 fc-cache -vf
+curl https://repo.continuum.io/archive/Anaconda2-5.1.0-MacOSX-x86_64.sh -o anaconda2.sh
